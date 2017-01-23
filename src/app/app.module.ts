3abe29e8c9138dc839c64d78f6b69c642c7ceac2
Ellie Angell and Ezra Sandzer-Bell <ezra@>
2017-01-23 10:08:20 -0800

    create working template with routing working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NewpostComponent } from './newpost/newpost.component';
@@ -16,7 +16,8 @@ import { NewpostComponent } from './newpost/newpost.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
